ed2afabd34c6cb68bd2486a9eaf5f6e479338e22
Natalie <nataliembatha@gmail.com>
2023-02-24 16:41:41 +0100

    file containing icon details

--- src/skillsData.js
+++ src/skillsData.js
@@ -1,16 +1,70 @@
+// const skills = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Bootstrap',
+//   'Tailwind CSS',
+//   'Flask',
+//   'Python',
+//   'Git',
+//   'Github',
+//   'Kanban',
+//   'Trello'
+// ]
+
 const skills = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Bootstrap',
-  'Tailwind CSS',
-  'Flask',
-  'Python',
-  'Git',
-  'Github',
-  'Kanban',
-  'Trello'
+  {
+    id: 1,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    description: "React"
+  },
+  {
+    id: 2,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    description: "JavaScript"
+  },
+  {
+    id: 3,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    description: "HTML5"
+  },
+  {
+    id: 4,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    description: "CSS3"
+  },
+  {
+    id: 5,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    description: "Bootstrap"
+  },
+  {
+    id: 6,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+    description: "Jest"
+  },
+  {
+    id: 7,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    description: "Python"
+  },
+  {
+    id: 8,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg",
+    description: "Trello"
+  },
+  {
+    id: 9,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+    description: "VSCode"
+  },
+  {
+    id: 10,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    description: "Git"
+  }
 ]
+
 
 export default skills;
