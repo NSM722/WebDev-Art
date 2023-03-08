c2ccc6c4436b2d688d84b9d965053d0a386f1bd7
Natalie <nataliembatha@gmail.com>
2023-03-08 12:53:29 +0100

    Added discernible texts to my links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,22 +6,26 @@ function Contact() {
       <section className='icons'>
         <div>
           <a href="https://www.linkedin.com/in/natalie-mbatha/" target="_blank"
-            rel="noreferrer"><i className="fa-brands fa-linkedin"></i>
+            rel="noreferrer"><i className="fa-brands fa-linkedin"
+              aria-label="Natalie Mbatha's LinkedIn profile"></i>
           </a>
         </div>
         <div>
           <a href="https://codepen.io/NatalieSM/pens/public" target="_blank"
-            rel="noreferrer"><i className="fa-brands fa-codepen"></i>
+            rel="noreferrer"><i className="fa-brands fa-codepen"
+              aria-label="Natalie Mbatha's Codepen profile"></i>
           </a>
         </div>
         <div>
           <a href="https://github.com/NSM722" target="_blank"
-            rel="noreferrer"><i className="fa-brands fa-github"></i>
+            rel="noreferrer"><i className="fa-brands fa-github"
+              aria-label="Natalie Mbatha's Github profile"></i>
           </a>
         </div>
         <div>
           <a href="mailto:nataliembatha@gmail.com" target="_blank"
-            rel="noreferrer"><i className="fa-regular fa-envelope"></i>
+            rel="noreferrer"><i className="fa-regular fa-envelope"
+              aria-label="Natalie Mbatha's Email address"></i>
           </a>
         </div>
       </section>
